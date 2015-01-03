5590e89497af6975269bf5cb385c191199175b81
TheBerkin <grenzatuna@gmail.com>
2015-01-03 07:38:19 -0600

    Add demo project

---
+++ Diffmark.Demo/DemoForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Diffmark.Demo
+{
+    partial class DemoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gbOutput = new System.Windows.Forms.GroupBox();
+            this.gbInput = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBaseString = new System.Windows.Forms.TextBox();
+            this.txtPattern = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.delayTimer = new System.Windows.Forms.Timer(this.components);
+            this.gbOutput.SuspendLayout();
+            this.gbInput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbOutput
+            // 
+            this.gbOutput.Controls.Add(this.txtOutput);
+            this.gbOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbOutput.Location = new System.Drawing.Point(0, 93);
+            this.gbOutput.Name = "gbOutput";
+            this.gbOutput.Size = new System.Drawing.Size(415, 149);
+            this.gbOutput.TabIndex = 0;
+            this.gbOutput.TabStop = false;
+            this.gbOutput.Text = "Output";
+            // 
+            // gbInput
+            // 
+            this.gbInput.Controls.Add(this.label2);
+            this.gbInput.Controls.Add(this.txtPattern);
+            this.gbInput.Controls.Add(this.txtBaseString);
+            this.gbInput.Controls.Add(this.label1);
+            this.gbInput.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gbInput.Location = new System.Drawing.Point(0, 0);
+            this.gbInput.Name = "gbInput";
+            this.gbInput.Size = new System.Drawing.Size(415, 93);
+            this.gbInput.TabIndex = 1;
+            this.gbInput.TabStop = false;
+            this.gbInput.Text = "Input";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Base string";
+            // 
+            // txtBaseString
+            // 
+            this.txtBaseString.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBaseString.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBaseString.Location = new System.Drawing.Point(94, 21);
+            this.txtBaseString.Name = "txtBaseString";
+            this.txtBaseString.Size = new System.Drawing.Size(309, 27);
+            this.txtBaseString.TabIndex = 1;
+            this.txtBaseString.TextChanged += new System.EventHandler(this.txtBaseString_TextChanged);
+            // 
+            // txtPattern
+            // 
+            this.txtPattern.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPattern.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPattern.Location = new System.Drawing.Point(94, 54);
+            this.txtPattern.Name = "txtPattern";
+            this.txtPattern.Size = new System.Drawing.Size(309, 27);
+            this.txtPattern.TabIndex = 2;
+            this.txtPattern.TextChanged += new System.EventHandler(this.txtPattern_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pattern";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOutput.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(3, 18);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(409, 128);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // delayTimer
+            // 
+            this.delayTimer.Tick += new System.EventHandler(this.delayTimer_Tick);
+            // 
+            // DemoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 242);
+            this.Controls.Add(this.gbOutput);
+            this.Controls.Add(this.gbInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "DemoForm";
+            this.Text = "Diffmark demo";
+            this.gbOutput.ResumeLayout(false);
+            this.gbOutput.PerformLayout();
+            this.gbInput.ResumeLayout(false);
+            this.gbInput.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbOutput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.GroupBox gbInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPattern;
+        private System.Windows.Forms.TextBox txtBaseString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer delayTimer;
+    }
+}
+
---
+++ Diffmark.Demo/DemoForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Diffmark.Demo
+{
+    public partial class DemoForm : Form
+    {
+        public DemoForm()
+        {
+            InitializeComponent();
+            txtBaseString.Text = "world";
+            txtPattern.Text = @"Hello,\s+ ; !";
+            ResetTimer();
+        }
+
+        private static readonly Color GoodColor = SystemColors.Control;
+        private static readonly Color BadColor = Color.OrangeRed;
+
+        private void delayTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {   
+                txtOutput.Text = Diff.Mark(txtBaseString.Text, txtPattern.Text);
+                txtOutput.ForeColor = GoodColor;
+            }
+            catch
+            {
+                txtOutput.ForeColor = BadColor;
+            }
+        }
+
+        private void txtBaseString_TextChanged(object sender, EventArgs e)
+        {
+            ResetTimer();
+        }
+
+        private void txtPattern_TextChanged(object sender, EventArgs e)
+        {
+            ResetTimer();
+        }
+
+        private void ResetTimer()
+        {
+            delayTimer.Stop();
+            delayTimer.Start();
+        }
+    }
+}
